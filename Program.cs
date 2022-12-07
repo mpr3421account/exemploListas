049328a62c5a676454ee784cf6dd4ef78f14eb22
Marcos Paulo <itsolutions031250@gmail.com>
2022-12-07 17:00:19 -0300

    list.FindAll

--- Program.cs
+++ Program.cs
@@ -20,7 +20,15 @@ Console.WriteLine("Firs 'A': " + s1);
 string s2 = list.FindLast(x => x[0] == 'A');
 Console.WriteLine("Last 'A':"+s2);
 
+//Encontrar primeira ou última posição de elemento da lista que satisfaça um predicado: list.FindIndex, list.FindLastIndex
+
 int pos1 = list.FindIndex(x => x[0] == 'A');
 Console.WriteLine("First position 'A':" + pos1);
 int pos2 = list.FindLastIndex(x => x[0] == 'A');
 Console.WriteLine("Last position 'A':" + pos2);
+
+List<string> list2 = list.FindAll(x => x.Length == 5);
+foreach (string obj in list2)
+{
+    Console.WriteLine("Obejto da lista com 5 caracteres:" + obj);
+}
