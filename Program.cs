eb4fc8c09e63850ea5c77d3e4eeb13be1e0aead5
Marcos Paulo <itsolutions031250@gmail.com>
2022-12-07 16:56:13 -0300

    list.FindIndex, list.FindLastIndex

--- Program.cs
+++ Program.cs
@@ -13,7 +13,14 @@ foreach(string obj in list)
 }
 Console.WriteLine("List count:" + list.Count);//Tamanho da lista: Count
 
+//Encontrar primeiro ou último elementos da lista que satisfaça um predicado:list.Find, list.FindLast
+
 string s1 = list.Find(x => x[0] == 'A');
 Console.WriteLine("Firs 'A': " + s1);
 string s2 = list.FindLast(x => x[0] == 'A');
 Console.WriteLine("Last 'A':"+s2);
+
+int pos1 = list.FindIndex(x => x[0] == 'A');
+Console.WriteLine("First position 'A':" + pos1);
+int pos2 = list.FindLastIndex(x => x[0] == 'A');
+Console.WriteLine("Last position 'A':" + pos2);
