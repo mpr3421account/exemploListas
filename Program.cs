6d2835e774f5627948d74fd4d9614214d0732aec
Marcos Paulo <itsolutions031250@gmail.com>
2022-12-07 16:01:59 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿List<string> list = new List<string>();
+list.Add("maria");
+list.Add("alex");
+list.Add("boba");
+list.Add("anna");
+
+
+
+foreach(string obj in list)
+{
+    Console.WriteLine(obj);
+}
